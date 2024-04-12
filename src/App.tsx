027b9f690454e886da589e4b4988e98cc832351b
Bogdan Daniel Ioan <bogdan.daniel.ioan@outlook.com>
2024-04-12 12:40:37 +0300

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LoginProvider from './components/context/LoginProvider';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './components/user/components/Login';
-import Register from './components/user/components/Register';
-import Home from './components/pages/Home';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 
+const Home = lazy(() => import('./components/pages/Home'));
+const Login = lazy(() => import('./components/user/components/Login'));
+const Register = lazy(() => import('./components/user/components/Register'));
 function App() {
-  return (
-    <LoginProvider>
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  </LoginProvider>
-  );
+    return (
+        <LoginProvider>
+            <div className="App">
+                <BrowserRouter basename="/ui">
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route path="*" element={<Navigate to="/" replace />} />
+                        </Routes>
+                    </Suspense>
+                </BrowserRouter>
+            </div>
+        </LoginProvider>
+    );
 }
+
 
 export default App;
--- src/components/utils/ApiServer.ts
+++ src/components/utils/ApiServer.ts
@@ -3,17 +3,17 @@ import HttpResponse from "../user/dto/HttpReponse";
 class ApiServer{
 
  api<U, T>(path: string, method: string, body: U, token: string): Promise<HttpResponse<T>> {
-  const url = "/server" + path;
+  const url = "http://localhost:5000/server" + path;
   const options: RequestInit = {
     method,
     mode: "cors",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
+        "Access-Control-Allow-Origin":"*",
       Authorization: `Bearer ${token}`,
     },
     body: body == null ? null : JSON.stringify(body),
   };
-
   return fetch(url, options);
 }
 }
